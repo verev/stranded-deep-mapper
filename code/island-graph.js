6b45d397cbe45417739d5b16738a3f51918aa4f6
verev <verev@gmx.com>
2015-02-04 22:33:46 +0200

    added head comment

---
+++ code/island-graph.js
@@ -0,0 +1,18 @@
+/* island-graph.js
+ *    manage internal representation of the map
+ *
+ * Copyright 2015. verev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
